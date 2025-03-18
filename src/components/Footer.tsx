214d71fbc4603815106385634cdec60c2705c331
Sanket Sawant <sanketsawant2210@gmail.com>
2025-03-18 23:45:48 +0530

    updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { FizziLogo } from "./FizziLogo";
 import CircleText from "./CirlceText";
 
-type Props = {};
 
 const Footer = () => {
   return (
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import  { FizziLogo }  from "@/components/FizziLogo"
 
-type Props = {}
-
 export default function Header() {
   return (
     <header className="flex justify-center py-4 -mb-28">
